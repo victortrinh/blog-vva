fb922dcebbc33cb14557b95af24ade86a017d2c4
Victor Trinh <victor.trinh@outlook.com>
2025-01-29 10:04:32 -0500

    Fix errors

--- src/app/resources/index.ts
+++ src/app/resources/index.ts
@@ -1,2 +1,2 @@
-export { routes, protectedRoutes, style, baseURL } from "@/app/resources/config"
+export { routes, protectedRoutes, baseURL } from "@/app/resources/config"
 export { renderContent } from "@/app/resources/renderContent";
